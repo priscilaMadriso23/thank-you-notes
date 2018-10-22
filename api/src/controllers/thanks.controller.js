9bd5bd493a16d541e04a4c700ea4d2942975c14f
Francisco Angulo D <ttymarucr@gmail.com>
2018-10-22 09:50:28 -0600

    added slack webhook response

--- api/src/controllers/thanks.controller.js
+++ api/src/controllers/thanks.controller.js
@@ -1,12 +1,19 @@
 const { splitTextMessage } = require('../utils/messageUtils');
 const { thanks } = require('../contracts/ThankYouNoteContract');
+const { call } = require('../utils/fetchHandler');
 
-exports.thanks = async (req, res) => {
+exports.thanks = (req, res) => {
   const { body } = req;
   const { user_name, text, team_domain } = body;
   try {
     const { username, message } = splitTextMessage(text);
-    await thanks(team_domain, user_name, username, message);
+    thanks(team_domain, user_name, username, message)
+      .then(() => {
+        call({
+          endpoint: team_domain,
+          body: { text: `<@${username}> just received 1 TYN !` },
+        });
+      });
     res.status(200).json({ text: `1 TYN sent to <@${username}> !` });
   } catch (error) {
     res.status(200).json({ text: error.message });
