d6b3dccc288b2f2e906892956190d051cb560a40
Francisco Angulo D <ttymarucr@gmail.com>
2018-11-21 11:36:29 -0600

    resolved an issue sending a callback to slack hook

--- api/src/controllers/thanks.controller.js
+++ api/src/controllers/thanks.controller.js
@@ -1,17 +1,20 @@
 const { splitTextMessage } = require('../utils/messageUtils');
 const { thanks } = require('../contracts/ThankYouNoteContract');
 const { call } = require('../utils/fetchHandler');
+const config = require('../config/config.json');
 
 exports.thanks = (req, res) => {
   const { body } = req;
   const { user_name, text, team_domain } = body;
   try {
     const { username, message } = splitTextMessage(text);
+    const { hook } = config[team_domain];
     thanks(team_domain, user_name, username, message)
       .then(() => {
         call({
-          endpoint: team_domain,
-          body: { text: `<@${username}> just received 1 TYN :tyn: !` },
+          endpoint: 'slackHooks',
+          url: hook,
+          body: { text: `<@${username}> just received 1 TYN :tyn: from ${user_name} !` },
         });
       });
     res.status(200).json({ text: `1 TYN :tyn: sent to <@${username}> !` });
